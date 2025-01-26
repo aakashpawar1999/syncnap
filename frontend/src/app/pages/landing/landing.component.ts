70121f8369fabb3d76286759a25b9cd84cfc2ea9
aakashpawar1999 <aakashpawar1999@gmail.com>
2025-01-26 13:29:35 +0530

    added seo service for meta tags

--- frontend/src/app/pages/landing/landing.component.ts
+++ frontend/src/app/pages/landing/landing.component.ts
@@ -7,6 +7,7 @@ import { CtaComponent } from '../../shared/components/cta/cta.component';
 import { PricingComponent } from '../../shared/components/pricing/pricing.component';
 import { FaqComponent } from '../../shared/components/faq/faq.component';
 import { TestimonialComponent } from '../../shared/components/testimonial/testimonial.component';
+import { SeoService } from '../../shared/services/seo.service';
 
 @Component({
   selector: 'app-landing',
@@ -25,7 +26,46 @@ import { TestimonialComponent } from '../../shared/components/testimonial/testim
   styleUrl: './landing.component.css',
 })
 export class LandingComponent implements OnInit {
-  constructor() {}
+  constructor(private seoService: SeoService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.setMetaData();
+  }
+
+  setMetaData() {
+    this.seoService.setMetaTags([{ name: 'robots', content: 'index, follow' }]);
+    this.seoService.setTitle('SyncNap');
+    this.seoService.setDescription(
+      'Simplify your workflows by connecting Supabase with Airtable in just a few clicks. Sync tables seamlessly and stay in control of your data.',
+    );
+    this.seoService.setMetaTags([
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:title', content: 'SyncNap' },
+      {
+        name: 'twitter:description',
+        content:
+          'Simplify your workflows by connecting Supabase with Airtable in just a few clicks. Sync tables seamlessly and stay in control of your data.',
+      },
+      // {
+      //   name: 'twitter:image',
+      //   content:
+      //     'https://',
+      // },
+    ]);
+    this.seoService.setMetaPropertyTags([
+      { property: 'og:title', content: 'SyncNap' },
+      {
+        property: 'og:description',
+        content:
+          'Simplify your workflows by connecting Supabase with Airtable in just a few clicks. Sync tables seamlessly and stay in control of your data.',
+      },
+      // {
+      //   property: 'og:image',
+      //   content:
+      //     'https://',
+      // },
+      { property: 'og:url', content: 'https://syncnap.com' },
+    ]);
+    this.seoService.setCanonicalURL('https://syncnap.com');
+  }
 }
